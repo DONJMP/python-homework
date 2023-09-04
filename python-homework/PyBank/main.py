6f3f361673a66a3e51871b71d504b21d3c7068cb
DONJMP <141264743+DONJMP@users.noreply.github.com>
2023-09-04 13:55:37 -0400

    Add files via upload

---
+++ python-homework/PyBank/main.py
@@ -0,0 +1,54 @@
+import csv
+from pathlib import Path
+
+budget_filepath = Path('budget_data.csv')
+output_file = Path('analysis/budget_analysis.txt')
+
+# Track various financial parameters
+total_months = 0
+month_of_change = []
+net_change_list = []
+greatest_increase = ["", 0]
+greatest_decrease = ["", 9999999999999999999]
+total_net = 0
+
+with open(budget_filepath) as budget:
+    reader = csv.reader(budget)
+    
+    header = next(reader)
+    # Extract first row to avoid appending to net_change_list
+    first_row = next(reader)
+    total_months = total_months + 1
+    total_net = total_net + int(first_row[1])
+    prev_net = int(first_row[1])
+
+    for row in reader:
+        # Track the total
+        total_months = total_months + 1
+        total_net = total_net + int(row[1])
+        # Track the net change
+        net_change = int(row[1]) - prev_net
+        prev_net = int(row[1])
+        net_change_list = net_change_list + [net_change]
+        month_of_change = month_of_change + [row[0]]
+        # Calculate the greatest increase
+        if net_change > greatest_increase[1]:
+            greatest_increase[0] = row[0]
+            greatest_increase[1] = net_change
+            # Calculate the greatest decrease
+        if net_change < greatest_decrease[1]:
+            greatest_decrease[0] = row[0]
+            greatest_decrease[1] = net_change
+
+# Calculate the Average Net Change
+net_monthly_avg = round(sum(net_change_list) / len(net_change_list),2)
+
+# Export the results to text file
+with open(output_file, "w") as txt_file:
+    txt_file.write(f"Financial Analysis\n")
+    txt_file.write(f"----------------------------\n")
+    txt_file.write(f"Total Months: {total_months}\n")
+    txt_file.write(f"Total: ${total_net}\n")
+    txt_file.write(f"Average  Change: ${net_monthly_avg}\n")
+    txt_file.write(f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n")
+    txt_file.write(f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
